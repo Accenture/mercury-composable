c89aea4f411f2295f2fbedcc6f38e87d6153453a
Eric Law <39393654+acn-ericlaw@users.noreply.github.com>
2025-12-11 06:25:05 -0800

    remove unused import

--- system/platform-core/src/main/java/org/platformlambda/core/models/InboxBase.java
+++ system/platform-core/src/main/java/org/platformlambda/core/models/InboxBase.java
@@ -31,7 +31,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This is reserved for system use.
